32932e33473966c16ceee7ae97b379aeea70e35d
Yuji Okazawa <yujiorama@gmail.com>
2019-01-29 09:29:52 +0900

    update

--- dot.bashrc.d/ruby.sh
+++ dot.bashrc.d/ruby.sh
@@ -4,12 +4,16 @@ if [[ -d "/c/tools/ruby26" ]]; then
     PATH=${PATH}:/c/tools/ruby26/bin
 fi
 
+if alias | grep -w be >/dev/null 2>&1; then unalias be; fi
+if alias | grep -w bundle >/dev/null 2>&1; then unalias bundle; fi
+
 if which bundle >/dev/null 2>&1; then
     alias be='bundle exec ' 
 else
     RUBY_ROOT_="$(dirname "$(which ruby)")"
     if [[ -e "${RUBY_ROOT_}/bundle.cmd" ]]; then
         alias be="${RUBY_ROOT_}/bundle.cmd exec "
+        alias bundle="${RUBY_ROOT_}/bundle.cmd "
     fi
     unset RUBY_ROOT_
 fi
