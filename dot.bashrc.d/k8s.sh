1287a2682db9cbe88c98c05d7cf65acf8d3f6787
Yuji Okazawa <yujiorama@gmail.com>
2019-11-13 16:32:32 +0900

    k8s: tune reconfigure script

--- dot.bashrc.d/k8s.sh
+++ dot.bashrc.d/k8s.sh
@@ -25,12 +25,47 @@ if command -v eksctl >/dev/null 2>&1; then
 fi
 
 k8s_reconfigure() {
-   local docker_host_ hostpart_
+  local docker_host hostpart docker_cert_path cert_path_parent ca_path client_crt_path client_key_path
 
-   docker_reconfigure
+  # shellcheck source=/dev/null
+  source "${HOME}/.bashrc.d/docker.env"
+  # shellcheck source=/dev/null
+  source "${HOME}/.bashrc.d/docker.sh"
 
-   docker_host_=$(echo "${DOCKER_HOST}" | cut -d ' ' -f 2 | cut -d '=' -f 2)
-   hostpart_=${docker_host_##tcp://}
-   hostpart_=${hostpart_%%:*}
-   kubectl config set-cluster minikube --server="https://${hostpart_}:8443"
+  docker_host=$(echo "${DOCKER_HOST}" | cut -d ' ' -f 2 | cut -d '=' -f 2)
+  if [[ "" = "${docker_host}" ]]; then
+    return
+  fi
+  hostpart=${docker_host##tcp://}
+  hostpart=${hostpart%%:*}
+
+  docker_cert_path=$(echo "${DOCKER_CERT_PATH}" | cut -d ' ' -f 2 | cut -d '=' -f 2)
+  if [[ "" = "${docker_cert_path}" ]]; then
+    return
+  fi
+  cert_path_parent=$(dirname "${docker_cert_path}")
+  ca_path="${cert_path_parent}"/ca.crt
+  if [[ ! -e "${ca_path}" ]]; then
+    return
+  fi
+  client_crt_path="${cert_path_parent}"/client.crt
+  if [[ ! -e "${client_crt_path}" ]]; then
+    return
+  fi
+  client_key_path="${cert_path_parent}"/client.key
+  if [[ ! -e "${client_key_path}" ]]; then
+    return
+  fi
+
+  kubectl config set-cluster minikube \
+    --embed-certs=true \
+    --server="https://${hostpart}:8443" \
+    --certificate-authority="${ca_path}"
+
+  kubectl config set-credentials minikube \
+    --embed-certs=true \
+    --client-certificate="${client_crt_path}" \
+    --client-key="${client_key_path}"
+
+  kubectl config use-context minikube
 }
