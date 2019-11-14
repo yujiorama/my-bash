624a167db7f43e7ccd7c3400d4a294572a8a0cb8
Yuji Okazawa <yujiorama@gmail.com>
2019-11-14 12:53:19 +0900

    wsl: tweak git-secrets procedure

--- wsl/.bashrc.d/git.sh
+++ wsl/.bashrc.d/git.sh
@@ -25,8 +25,12 @@ fi
 
 mkdir -p "${HOME}/.git-secrets" "${HOME}/.git-templates"
 download_new_file "https://raw.githubusercontent.com/awslabs/git-secrets/master/git-secrets" "${HOME}/.git-secrets/git-secrets"
+chmod 755 "${HOME}/.git-secrets/git-secrets"
 download_new_file "https://raw.githubusercontent.com/awslabs/git-secrets/master/git-secrets.1" "${HOME}/.git-secrets/git-secrets.1"
 
 echo run git secrets --install -f "${HOME}/.git-templates"
 echo run git config --global init.templateDir '${HOME}/.git-templates/git-secrets'
 echo run git secrets --register-aws --global
+
+export PATH
+PATH="${HOME}/.git-secrets/git-secrets":${PATH}
