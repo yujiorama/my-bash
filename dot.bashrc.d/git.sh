5d991b835813ea72c16cff183308d12f55be12d3
Yuji Okazawa <yujiorama@gmail.com>
2019-11-14 12:48:08 +0900

    git: add git-secrets procedure

--- dot.bashrc.d/git.sh
+++ dot.bashrc.d/git.sh
@@ -8,3 +8,11 @@ if [[ -e /mingw64/share/git/completion/git-prompt.sh ]]; then
     # shellcheck source=/dev/null
     source "/mingw64/share/git/completion/git-prompt.sh"
 fi
+
+mkdir -p "${HOME}/.git-secrets" "${HOME}/.git-templates"
+download_new_file "https://raw.githubusercontent.com/awslabs/git-secrets/master/git-secrets" "${HOME}/.git-secrets/git-secrets"
+download_new_file "https://raw.githubusercontent.com/awslabs/git-secrets/master/git-secrets.1" "${HOME}/.git-secrets/git-secrets.1"
+
+echo run git secrets --install -f "${HOME}/.git-templates"
+echo run git config --global init.templateDir '${HOME}/.git-templates/git-secrets'
+echo run git secrets --register-aws --global
