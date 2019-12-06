8d121fa4a6ece1d29a834fe38a1b96f6ca000101
Yuji Okazawa <yujiorama@gmail.com>
2019-12-06 19:10:19 +0900

    gnupg: tweak

--- dot.bashrc.d/gnupg.sh
+++ dot.bashrc.d/gnupg.sh
@@ -1,10 +1,10 @@
 # vi: ai et ts=4 sw=4 sts=4 expandtab fs=shell
 
-if ! type gpg-agent >/dev/null 2>&1; then
+if ! command -v gpg-agent >/dev/null 2>&1; then
     return
 fi
 
-if ! type gpg-connect-agent >/dev/null 2>&1; then
+if ! command -v gpg-connect-agent >/dev/null 2>&1; then
     return
 fi
 
@@ -12,12 +12,12 @@ if ! gpg-agent 2>/dev/null; then
     mkdir -p "${GNUPGHOME}"
 
     /bin/cat - > "${GNUPGHOME}/gpg-agent.conf" << EOS
-    log-file gpg-agent.log
-    enable-putty-support
-    default-cache-ttl     86400
-    max-cache-ttl         86400
-    default-cache-ttl-ssh 86400
-    max-cache-ttl-ssh     86400
+log-file gpg-agent.log
+enable-putty-support
+default-cache-ttl     86400
+max-cache-ttl         86400
+default-cache-ttl-ssh 86400
+max-cache-ttl-ssh     86400
 EOS
 
     gpg-connect-agent --homedir "${GNUPGHOME}" killagent '//bye'
