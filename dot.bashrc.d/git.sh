1d1171133297c32569c671f899e69b3b0b9a9231
Yuji Okazawa <yujiorama@gmail.com>
2019-12-12 10:15:17 +0900

    git: deploy git-secrets to /mingw64/bin

--- dot.bashrc.d/git.sh
+++ dot.bashrc.d/git.sh
@@ -1,7 +1,7 @@
 # vi: ai et ts=4 sw=4 sts=4 expandtab fs=shell
 
 export PATH
-PATH="/mingw64/libexec/git-core:${PATH}"
+PATH="${PATH}:/mingw64/libexec/git-core"
 
 if [[ -e ${HOME}/git-prompt.sh ]]; then
     # shellcheck source=/dev/null
@@ -12,9 +12,9 @@ if [[ -e /mingw64/share/git/completion/git-prompt.sh ]]; then
     source "/mingw64/share/git/completion/git-prompt.sh"
 fi
 
-mkdir -p "${HOME}/.git-secrets" "${HOME}/.git-templates" "${HOME}/man/man1"
-download_new_file "https://raw.githubusercontent.com/awslabs/git-secrets/master/git-secrets" "${HOME}/.git-secrets/git-secrets"
-chmod 755 "${HOME}/.git-secrets/git-secrets"
+mkdir -p "${HOME}/.git-templates" "${HOME}/man/man1"
+download_new_file "https://raw.githubusercontent.com/awslabs/git-secrets/master/git-secrets" "/mingw64/bin/git-secrets"
+chmod 755 "/mingw64/bin/git-secrets"
 download_new_file "https://raw.githubusercontent.com/awslabs/git-secrets/master/git-secrets.1" "${HOME}/man/man1/git-secrets.1"
 
 if ! another_console_exists; then
@@ -22,9 +22,6 @@ if ! another_console_exists; then
     echo run git config --global init.templateDir "${HOME}/.git-templates/git-secrets"
     echo run git secrets --register-aws --global
 fi
-
-export PATH
-PATH="${HOME}/.git-secrets":${PATH}
 
 export MANPATH
 MANPATH=${HOME}/man/man1:${MANPATH}
