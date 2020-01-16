d04473a43c838261919646e604ec88daa5760e45
Yuji Okazawa <yujiorama@gmail.com>
2020-01-16 10:15:51 +0900

    python: tune path

--- dot.bashrc.d/python.sh
+++ dot.bashrc.d/python.sh
@@ -1,5 +1,9 @@
 # vi: ai et ts=4 sw=4 sts=4 expandtab fs=shell
 
+if ! command -v "${PYTHON}" >/dev/null 2>&1; then
+	return
+fi
+
 if command -v aws_completer >/dev/null 2>&1; then
     complete -C aws_completer aws
 fi
