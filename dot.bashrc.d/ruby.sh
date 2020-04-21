74d8159107f9e4245cf58be29cefda2b125f0c14
Yuji Okazawa <yujiorama@gmail.com>
2020-04-21 09:59:09 +0900

    ruby: install function

--- dot.bashrc.d/ruby.sh
+++ dot.bashrc.d/ruby.sh
@@ -1,27 +1,54 @@
 #!/bin/bash
 
 function ruby-install {
-    local version
-    version="${1:-2.7.1}"
     if [[ "${OS}" != "Linux" ]]; then
         return
     fi
+
+    local version
+    version="${1:-2.7.1}"
 
     if ! command -v git >/dev/null 2>&1; then
         return
     fi
 
-    if online github.com 443; then
-        sudo apt install -y libssl-dev libreadline-dev zlib1g-dev
-        git clone https://github.com/rbenv/rbenv.git "${HOME}/.rbenv"
-        (cd "${HOME}/.rbenv" && ./src/configure && make -C src)
-
-        # shellcheck disable=SC1090
-        source "${HOME}/.bashrc.d/ruby.sh"
-
-        mkdir -p "$(rbenv root)"/plugins
-        git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
+    if ! online github.com 443; then
+        return
     fi
+
+    sudo apt install -y \
+        libssl-dev \
+        libreadline-dev \
+        zlib1g-dev \
+        libyaml-dev \
+        libreadline-dev \
+        libncurses5-dev \
+        libffi-dev \
+        libgdbm-dev \
+        autoconf \
+        bison \
+        build-essential
+
+    if [[ ! -d "${HOME}/.rbenv" ]] || [[ ! -d "${HOME}/.rbenv/.git" ]]; then
+        git clone https://github.com/rbenv/rbenv.git "${HOME}/.rbenv"
+    else
+        (cd "${HOME}/.rbenv" && git pull)
+    fi
+    if [[ ! -d "${HOME}/.rbenv" ]] || [[ ! -d "${HOME}/.rbenv/.git" ]]; then
+        return
+    fi
+    (cd "${HOME}/.rbenv" && ./src/configure && make -C src)
+
+    # shellcheck disable=SC1090
+    source <("${HOME}/.rbenv/bin/rbenv" init -)
+
+    mkdir -p "$(rbenv root)"/plugins
+    if [[ ! -d "$(rbenv root)/plugins/ruby-build" ]]; then
+        git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
+    else
+        (cd "$(rbenv root)/plugins/ruby-build" && git pull)
+    fi
+
     if rbenv install --list | grep "${version}"; then
         rbenv install "${version}"
         rbenv local "${version}"
