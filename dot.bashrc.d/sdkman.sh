e96085a347506f225b069e24acb9d08e7b727973
Yuji Okazawa <yujiorama@gmail.com>
2020-05-11 08:16:29 +0900

    sdkman: tweak

--- dot.bashrc.d/sdkman.sh
+++ dot.bashrc.d/sdkman.sh
@@ -20,3 +20,24 @@ if [[ -e "${SDKMAN_DIR}/bin/sdkman-init.sh" ]]; then
     # shellcheck disable=SC1090
     source "${SDKMAN_DIR}/bin/sdkman-init.sh"
 fi
+
+if ! command -v sdk >/dev/null 2>&1; then
+    return
+fi
+
+cat - <<EOS > "${SDKMAN_DIR}/etc/config"
+sdkman_auto_answer=true
+sdkman_auto_selfupdate=true
+sdkman_insecure_ssl=false
+sdkman_curl_connect_timeout=7
+sdkman_curl_max_time=10
+sdkman_beta_channel=false
+sdkman_debug_mode=false
+sdkman_colour_enable=true
+sdkman_auto_env=false
+EOS
+
+for v in maven gradle; do
+    sdk install "${v}"
+    sdk current "${v}"
+done
