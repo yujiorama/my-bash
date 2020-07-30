8242e8a2b791aea11236c149a1f98ace45b97c8d
Yuji Okazawa <yujiorama@gmail.com>
2020-07-31 08:49:21 +0900

    remove unsed files

--- bin/switchdns.ps1
+++
@@ -1,5 +0,0 @@
-Get-DnsClientServerAddress -InterfaceIndex 14
-Set-DnsClientServerAddress -InterfaceIndex 14 -ServerAddresses "8.8.8.8,8.8.4.4"
-Get-DnsClientServerAddress -InterfaceIndex 14
-
-# "172.17.145.24,172.17.145.25"
