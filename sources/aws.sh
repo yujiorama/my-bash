d91417545d4bb3aeaeb06d5cd345d5fbeb632288
Yuji Okazawa <yujiorama@gmail.com>
2021-08-27 13:28:19 +0900

    aws: utility function

--- sources/aws.sh
+++ sources/aws.sh
@@ -65,12 +65,73 @@ function awscli-ssm-plugin-install {
         rm -f "${install_file}"
     fi
 
-    # shellcheck disable=SC1090
-    source "${MY_BASH_SOURCES}/aws.env"
-    # shellcheck disable=SC1090
-    source "${MY_BASH_SOURCES}/aws.sh"
+    # shellcheck disable=SC1091
+    [[ -e "${MY_BASH_SOURCES}/aws.env" ]] && source "${MY_BASH_SOURCES}/aws.env"
+    # shellcheck disable=SC1091
+    [[ -e "${MY_BASH_SOURCES}/aws.sh" ]] && source "${MY_BASH_SOURCES}/aws.sh"
 
     command -v session-manager-plugin
+}
+
+function aws-s3-bucket-delete() {
+    local bucket
+    bucket="$1"
+    if [[ -z "${bucket}" ]]; then
+        return 1
+    fi
+    if ! command -v aws >/dev/null 2>&1; then
+        return 1
+    fi
+    if ! command -v jq >/dev/null 2>&1; then
+        return 1
+    fi
+
+    if ! aws s3api put-bucket-versioning --bucket "${bucket}" --versioning-configuration Status=Suspended; then
+        return 1
+    fi
+
+    local delete_json
+    delete_json=$(basename "$(mktemp -p "${PWD}")")
+    aws s3api list-objects --bucket "${bucket}" | jq -r '{"Objects": [.Contents[] | {"Key": .Key}], "Quiet": false }' > "${delete_json}"
+    if [[ ! -s "${delete_json}" ]]; then
+        rm -f "${delete_json}"
+        return 1
+    fi
+
+    (
+        trap "rm -v ${delete_json}" 0 1 2 3 15
+        aws s3api delete-objects --bucket "${bucket}" --delete "file://${delete_json}"
+        aws s3api delete-bucket --bucket "${bucket}"
+    ) | cat -
+
+    return 0
+}
+
+function aws-ecr-repository-delete() {
+    local repository
+    repository="$1"
+    if [[ -z "${repository}" ]]; then
+        return 1
+    fi
+    if ! command -v aws >/dev/null 2>&1; then
+        return 1
+    fi
+    if ! command -v jq >/dev/null 2>&1; then
+        return 1
+    fi
+    local images
+    images=$(aws ecr list-images --repository-name "${repository}" --query 'imageIds[].imageTag' --output json | jq -r 'map("imageTag=" + .) | join(" ")')
+    if [[ -z "${images}" ]]; then
+        return 1
+    fi
+
+    # shellcheck disable=SC2086
+    if ! aws ecr batch-delete-image --repository "${repository}" --image-ids ${images}; then
+        return 1
+    fi
+
+    aws ecr delete-repository --repository "${repository}"
+    return 0
 }
 
 
